0e99a9ddf9a422ce379dc6cb342ef42c7b042f6e
Tomsiaudvytis <tomsiaudvytis@gmail.com>
2018-01-15 09:40:51 +0200

    Testing update

--- DataAccessLayer/Helper.cs
+++ DataAccessLayer/Helper.cs
@@ -22,19 +22,38 @@
         }
 
         //Create DB with 3 tables in it.
-        private static void CreateDatabase(string sqliteFile)
+        public static void CreateDatabase(string sqliteFile)
         {
+            if (File.Exists(sqliteFile))
+            {
+                DeleteDatabase(sqliteFile);
+            }
+
             SQLiteConnection.CreateFile(sqliteFile);
 
-            var sqlConn = new SQLiteConnection("Data Source=" + sqliteFile); // connect to database
-            sqlConn.Open();
+            using (var sqlConn = new SQLiteConnection("Data Source=" + sqliteFile))
+            {
+                sqlConn.Open();
+                string query = $@"CREATE TABLE IF NOT EXISTS Dates ( SF_Number TEXT (100) CONSTRAINT PK_Dates PRIMARY KEY NOT NULL, Date TEXT (50)  NOT NULL );";
+                query += $@"CREATE TABLE IF NOT EXISTS ProductItem (Product_Item_Id TEXT (100) CONSTRAINT PK_ProductItem PRIMARY KEY NOT NULL,Product_Name TEXT (100) NOT NULL, Unit TEXT (15) NOT NULL );";
+                query += $@"CREATE TABLE IF NOT EXISTS Products ( Product_Item_Id TEXT (100) NOT NULL CONSTRAINT FK_PRODUCTS_ITEM_ID REFERENCES ProductItem (Product_Item_Id), SF_Number TEXT(100) CONSTRAINT FR_Dates_SFNUMBER REFERENCES Dates (SF_Number) NOT NULL,Price DECIMAL (15) NOT NULL, Quantity TEXT (100) NOT NULL );";
+                using (var command = new SQLiteCommand(query, sqlConn))
+                {
+                    command.ExecuteNonQuery();
+                    sqlConn.Dispose();
+                }
+            };
 
-            string query = $@"CREATE TABLE IF NOT EXISTS Dates ( SF_Number TEXT (100) CONSTRAINT PK_Dates PRIMARY KEY NOT NULL, Date TEXT (50)  NOT NULL );";
-            query += $@"CREATE TABLE IF NOT EXISTS ProductItem (Product_Item_Id TEXT (100) CONSTRAINT PK_ProductItem PRIMARY KEY NOT NULL,Product_Name TEXT (100) NOT NULL, Unit TEXT (15) NOT NULL );";
-            query += $@"CREATE TABLE IF NOT EXISTS Products ( Product_Item_Id TEXT (100) NOT NULL CONSTRAINT FK_PRODUCTS_ITEM_ID REFERENCES ProductItem (Product_Item_Id), SF_Number TEXT(100) CONSTRAINT FR_Dates_SFNUMBER REFERENCES Dates (SF_Number) NOT NULL,Price DECIMAL (15) NOT NULL, Quantity TEXT (100) NOT NULL );";
-            SQLiteCommand command = new SQLiteCommand(query, sqlConn);
-            command.ExecuteNonQuery();
-            sqlConn.Close();
+            //Sql lite does not release db properly..
+            GC.Collect();
+        }
+
+        public static void DeleteDatabase(string sqliteFile)
+        {
+            if (File.Exists(sqliteFile))
+            {
+                File.Delete(sqliteFile);
+            }
         }
     }
 }
--- Tests/DataAccessLayerTests/SQLiteRepositoryTests.cs
+++ Tests/DataAccessLayerTests/SQLiteRepositoryTests.cs
@@ -1,15 +1,30 @@
-﻿using DataAccessLayer.Models;
-using DataAccessLayer.Repositories;
+﻿using DataAccessLayer;
+using DataAccessLayer.Models;
 using DataAccessLayer.Repositories.Interfaces;
 using FakeItEasy;
 using NLog;
 using NUnit.Framework;
+using System;
 
 namespace Tests.DataAccessLayerTests
 {
     [TestFixture]
     public class SQLiteRepositoryTests
     {
+        string dbPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop, Environment.SpecialFolderOption.Create) + $@"\TestSqlRepoDb.db";
+
+        [SetUp]
+        public void InitTestDb()
+        {
+            Helper.CreateDatabase(dbPath);
+        }
+
+        [TearDown]
+        public void DeleteTestDatabase()
+        {
+            Helper.DeleteDatabase(dbPath);
+        }
+
         [Test]
         public void AddProductItem_ShouldCallAddFromSqliteRepository()
         {
